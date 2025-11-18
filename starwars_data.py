3dac44c9d74e8e2a9ccc5e6cc626ef9e87ad857d
Camila Viñeg <vinegcamila27@gmail.com>
2025-11-17 23:59:57 -0300

    ejercicio 2do parcial y librerias necesarias

---
+++ starwars_data.py
@@ -0,0 +1,40 @@
+# personajes y episodios (episodios numerados 1..9)
+personajes = [
+    "Luke Skywalker", "Darth Vader", "Yoda", "Boba Fett", "C-3PO", "Leia Organa",
+    "Rey", "Kylo Ren", "Chewbacca", "Han Solo", "R2-D2", "BB-8",
+    "Obi-Wan Kenobi", "Anakin Skywalker", "Padmé Amidala", "Palpatine",
+    "Lando Calrissian", "Finn", "Poe Dameron", "Rose Tico",
+    "Qui-Gon Jinn", "Mace Windu", "Jango Fett", "Count Dooku", "General Grievous",
+    "Darth Maul", "Maz Kanata", "Captain Phasma"
+]
+
+apariciones = {
+    "Luke Skywalker":    [4,5,6,7,8,9],
+    "Darth Vader":       [1,2,3,4,5,6],    
+    "Yoda":              [1,2,3,4,5,6,8],
+    "Boba Fett":         [2,5,6],
+    "C-3PO":             [1,2,3,4,5,6,7,8,9],
+    "Leia Organa":       [4,5,6,7,8,9],
+    "Rey":               [7,8,9],
+    "Kylo Ren":          [7,8,9],
+    "Chewbacca":         [3,4,5,6,7,8,9],
+    "Han Solo":          [4,5,6,7],
+    "R2-D2":             [1,2,3,4,5,6,7,8,9],
+    "BB-8":              [7,8,9],
+    "Obi-Wan Kenobi":    [1,2,3,4],
+    "Anakin Skywalker":  [1,2,3],
+    "Padmé Amidala":     [1,2,3],
+    "Palpatine":         [1,2,3,4,5,6,9],
+    "Lando Calrissian":  [5,6,9],
+    "Finn":              [7,8,9],
+    "Poe Dameron":       [7,8,9],
+    "Rose Tico":         [8,9],
+    "Qui-Gon Jinn":      [1],
+    "Mace Windu":        [1,2,3],
+    "Jango Fett":        [2],
+    "Count Dooku":       [2,3],
+    "General Grievous":  [3],
+    "Darth Maul":        [1],
+    "Maz Kanata":        [7,8],
+    "Captain Phasma":    [7,8]
+}
